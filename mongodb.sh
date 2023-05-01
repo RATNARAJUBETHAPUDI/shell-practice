a2a1674a29a60d5d8e316d0b6ced271211e6d7d6
RATNARAJUBETHAPUDI <ratnaesi008@gmail.com>
2023-05-01 21:39:12 +0530

    create frontend scripts & conf files with records

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,8 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
+
+ sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
 systemctl enable mongod
-systemctl start mongod
-## edit the file and replace it with different ip from 127.0.0.1 to 0.0.0.0
+systemctl restart mongod
+
